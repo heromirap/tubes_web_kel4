00e2d154cf26938059408e31ab12ab14b6ab4ddf
Ridhosbr <ridhoperlinta06@gmail.com>
2025-05-30 23:41:54 +0700

    cobaa

---
+++ database/factories/LapanganFactory.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Lapangan;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class PenyewaanFactory extends Factory
+{
+  /**
+   * Define the model's default state.
+   *
+   * @return array
+   */
+  public function definition()
+  {
+  }
+}
--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -3,7 +3,7 @@
 namespace Database\Factories;
 
 use Illuminate\Database\Eloquent\Factories\Factory;
-use Illuminate\Support\Facades\Hash;
+//use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Str;
 
 /**
@@ -19,26 +19,32 @@ class UserFactory extends Factory
     /**
      * Define the model's default state.
      *
-     * @return array<string, mixed>
+     * @return array
      */
-    public function definition(): array
+    public function definition()
     {
+        $name = $this->faker->name();
+        $role = collect(['1', '2'])->random();
+
         return [
-            'name' => fake()->name(),
-            'email' => fake()->unique()->safeEmail(),
-            'email_verified_at' => now(),
-            'password' => static::$password ??= Hash::make('password'),
-            'remember_token' => Str::random(10),
+        'username' => $this->faker->userName(),
+        'name' => $name,
+        'email' => $this->faker->unique()->safeEmail(),
+        'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
+        'role' => $role,
+        'remember_token' => Str::random(10),
         ];
     }
 
     /**
      * Indicate that the model's email address should be unverified.
      */
-    public function unverified(): static
+    public function unverified()
     {
-        return $this->state(fn (array $attributes) => [
-            'email_verified_at' => null,
-        ]);
+         return $this->state(function (array $attributes) {
+      return [
+        'email_verified_at' => null,
+      ];
+    });
     }
 }
