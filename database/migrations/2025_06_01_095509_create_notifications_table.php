950a1ba907613d6ca138566575e06c376ee38206
Ridhosbr <ridhoperlinta06@gmail.com>
2025-06-01 17:23:50 +0700

    database

--- database/migrations/2025_06_01_095509_create_notifications_table.php
+++ database/migrations/2025_06_01_095509_create_notifications_table.php
@@ -4,24 +4,30 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
+class CreateNotificationsTable extends Migration
 {
     /**
      * Run the migrations.
+     *
+     * @return void
      */
-    public function up(): void
+    public function up()
     {
         Schema::create('notifications', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('id_user')->constrained('users')->cascadeOnDelete();
+            $table->text('data');
             $table->timestamps();
         });
     }
 
     /**
      * Reverse the migrations.
+     *
+     * @return void
      */
-    public function down(): void
+    public function down()
     {
         Schema::dropIfExists('notifications');
     }
-};
+}
