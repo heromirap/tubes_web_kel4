d8d17262131d2e0c2186661ad82474f930b0729a
heromirap <heromirapita.2744@gmail.com>
2025-06-05 22:47:55 +0700

    store request lapangan

---
+++ app/Http/Requests/Lapangan/StoreRequest.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Requests\Lapangan;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class StoreRequest extends FormRequest
+{
+  /**
+   * Determine if the user is authorized to make this request.
+   *
+   * @return bool
+   */
+  public function authorize()
+  {
+    return auth()->check();
+  }
+
+  /**
+   * Get the validation rules that apply to the request.
+   *
+   * @return array
+   */
+  public function rules()
+  {
+    return [
+      'no_lapangan' => 'required',
+      'harga_per_jam' => 'required|numeric',
+      'deskripsi' => 'required',
+      'status' => 'required',
+    ];
+  }
+}
